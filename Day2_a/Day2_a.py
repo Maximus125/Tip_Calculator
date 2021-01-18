75106ac2f4f7999ef26c283d5cebebb503225b24
Maximus125 <abhioverlord@outlook.com>
2021-01-18 19:30:23 +0530

    Add project files.

---
+++ Day2_a/Day2_a.py
@@ -0,0 +1,10 @@
+﻿#Tip Calculator
+print("Welcome to Tip Calculator")
+
+bill = float(input("What was your total bill?  $"))
+tip = int(input("What percentage tip would you like to give? 10, 12 or 15\n"))
+people = int(input("How many people to split the bill?\n"))
+
+new_bill = (bill+((tip/100)*bill))/people
+
+print(f"Each person should pay: ${round(new_bill,2)}.")
